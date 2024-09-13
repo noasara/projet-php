171881cdd403cda5d6c3ab7104396427999ba4a1
Catcathg <catherine-guo@outlook.fr>
2024-09-13 15:46:01 +0200

    dushsldjkg

--- ajoutFilm.php
+++ ajoutFilm.php
@@ -6,6 +6,7 @@ include "connectBDD.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="ajoutFilm.css" rel="stylesheet" type="text/css" />
     <title>Ajout d'un film</title>
 </head>
 <body>
